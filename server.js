d2f7dcf9d1c5f36deee5ea6eb36da0123c30cd59
enes uludag <enes.uludag.1996@gmail.com>
2022-08-05 00:36:21 +0300

    .

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const axios = require('axios');
 const app = express()
 const jenkinsUrl =process.env.JENKINS_URL;
-const jenkinsToken=process.env.JENKINS_TOKEN;
 const channelId=process.env.CHANNEL_ID;
 const port = process.env.PORT || 3000;
 const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.DirectMessages,GatewayIntentBits.DirectMessageTyping,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,GatewayIntentBits.DirectMessageReactions] });
@@ -44,12 +43,10 @@ client.login(process.env.BOT_TOKEN);
 app.get('/', (req, res) => {
     res.send('hello world')
   });
-  app.post('/build', (req, res) => {
+app.post('/build', (req, res) => {
     const {link,name} = req.body;
-    console.log((`${link} and ${name}`))
-   /*  client.channels.cache.get(channelId).send(` ${req.body.name} Build successfully!`);
-    client.channels.cache.get(channelId).send(`${req.body.link}`) */
-
+    client.channels.cache.get(channelId).send(` ${name} Build successfully!`);
+    client.channels.cache.get(channelId).send(`${link}`) 
     res.send('Build success')
   })
   app.listen(port, () => console.log('Build app listening on port 3000!'))
